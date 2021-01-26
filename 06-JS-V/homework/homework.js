b9080e64516b77c8cff78a44e3a2dc69f7d8248f
sanchezzfacu <sanchezzfacu>
2021-01-25 21:18:31 -0300

    CP commiteado por sanchezzfacu a las 1611620310793

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,8 +8,17 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-
-}
+  function Usuario(opciones, usuario, nombre, email, password) {
+      this.usuario = usuario;
+      this.nombre = nombre; 
+      this.email = email;
+      this.password = password;
+  }
+  let nuevoUsuario = new Usuario('Facundozz', 'Facundo', 'facundo@gmail.com', 'loremipsum')
+  Usuario.prototype.saludar = function () {
+    return 'Hola, mi nombre es ' + this.nombre 
+  }
+} 
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
